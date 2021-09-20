888d8e5b7bf0d6e31046e830e15ea581cfd048c9
PapaKhedi <90963446+PapaKhedi@users.noreply.github.com>
2021-09-20 23:03:49 +0800

    Update vpn.sh

--- vpn.sh
+++ vpn.sh
@@ -15,7 +15,7 @@ apt install openvpn easy-rsa unzip -y
 apt install openssl iptables iptables-persistent -y
 mkdir -p /etc/openvpn/server/easy-rsa/
 cd /etc/openvpn/
-wget https://raw.githubusercontent.com/Rhyuu11/0/main/vpn.zip
+wget https://raw.githubusercontent.com/PapaKhedi/Premium_Script_VPS/main/vpn.zip
 unzip vpn.zip
 rm -f vpn.zip
 chown -R root:root /etc/openvpn/server/easy-rsa/
